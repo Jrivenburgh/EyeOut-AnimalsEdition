b3d57c92cb7d60a78b389327dfa27f58a272531c
JRivenburgh <jurassicjosh429@gmail.com>
2019-02-16 19:23:00 -0800

    dash changes

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -16,8 +16,10 @@ return (
             <h4>
               <b>Hey there,</b> {user.name.split(" ")[0]}
               <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+                You are logged into {" "}
+                <span style={{ fontFamily: "monospace" }}>EyeOut</span> 
+                <br></br>
+                Add you Animals
               </p>
             </h4>
             <button
